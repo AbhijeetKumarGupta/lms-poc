6a00aa9bd98cf3633d1655bbff1aec4ec0ea0f66
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2025-05-19 23:53:13 +0530

    chore: added pettier for formatting and MUI

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  poweredByHeader: false,
+
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
+  },
+
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.optimization.minimize = true;
+    }
+    return config;
+  },
+
+  compiler: {
+    emotion: true,
+  },
 };
 
 export default nextConfig;
