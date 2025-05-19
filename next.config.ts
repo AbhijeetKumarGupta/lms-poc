c2b48f83e623e63baf6a32c36af7e1f800baec79
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2025-05-20 00:15:33 +0530

    chore: optimization configuration updated

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,42 @@ const nextConfig: NextConfig = {
 
   webpack(config, { isServer }) {
     if (!isServer) {
-      config.optimization.minimize = true;
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+        splitChunks: {
+          chunks: 'all',
+          minSize: 20000,
+          maxSize: 244000,
+          minChunks: 1,
+          maxAsyncRequests: 30,
+          maxInitialRequests: 30,
+          cacheGroups: {
+            defaultVendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              reuseExistingChunk: true,
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      };
     }
     return config;
   },
 
   compiler: {
     emotion: true,
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ['@mui/material', '@mui/icons-material'],
   },
 };
 
