00d53131b4563cdf74076ac2df4e61044a0a14e5
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2025-05-20 16:11:06 +0530

    chore: minor fixes

--- src/components/theme-test/ThemeTest.tsx
+++ src/components/theme-test/ThemeTest.tsx
@@ -18,7 +18,7 @@ export const ThemeTest = () => {
           <Typography variant="h5">Lizard</Typography>
           <Typography variant="body2">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s, when an unknown
+            has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book.
           </Typography>
         </CardContent>
